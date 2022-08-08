a3150f2890a441bb980a3ac1f2eac3b55fae1b01
roxsampoerna <110806380+roxsampoerna@users.noreply.github.com>
2022-08-08 14:15:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app = express()
 
 const host = process.env.HOST || undefined
-const port = parseInt(process.env.PORT ?? 8000)
+const port = parseInt(3000)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
